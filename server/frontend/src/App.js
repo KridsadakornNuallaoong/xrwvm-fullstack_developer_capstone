64ac3c586a58e103efcc29ffeac9da55bfc939ee
Kidsadakorn Nua <121489701+KridsadakornNuallaoong@users.noreply.github.com>
2026-01-01 02:31:50 +0700

    Lab: Add Dynamic Pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import Dealer from "./components/Dealers/Dealer";
+import Dealers from './components/Dealers/Dealers';
+import PostReview from "./components/Dealers/PostReview";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
 
@@ -9,6 +12,12 @@ function App() {
       <Route path="/register" element={<Register />} />
       {/* Login */}
       <Route path="/login" element={<LoginPanel />} />
+      {/* Dealers */}
+      <Route path="/dealers" element={<Dealers/>} />
+      {/* Dealer */}
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      {/* Post Review */}
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
